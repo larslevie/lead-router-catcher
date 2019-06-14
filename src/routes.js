24bc3371572bbd1d22c56054c89f7f1a73e61305
Lars Levie <larslevie@gmail.com>
2019-06-14 12:39:39 -0500

    Wrong response code

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const router = new Router();
 router.get("/healthz", ctx => (ctx.status = 204));
 router.post("/leads", ctx => {
   logger.info({ lead: ctx.request.body });
-  ctx.status = 204;
+  ctx.status = 200;
 });
 
 module.exports = router.routes();
