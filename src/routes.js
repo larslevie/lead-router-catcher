066480d610a6dccf395ddd61c05aa5c7120c6009
Lars Levie <larslevie@gmail.com>
2019-10-25 16:46:14 -0500

    Log with console

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const router = new Router();
 router.get("/healthz", ctx => (ctx.status = 204));
 router.post("/leads", ctx => {
   logger.info({ lead: ctx.request.body });
+  console.log({ lead: ctx.request.body });
   ctx.status = 200;
 });
 
