4e2ec3dbd51d9c2c7414dd9a392ac72971341214
Lars Levie <larslevie@gmail.com>
2020-06-22 10:01:41 -0500

    Add root route that logs entire request

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,14 @@ const logger = require("./logger");
 
 const router = new Router();
 
+router.get("/", ctx => {
+  logger.info({ req: ctx.request });
+  ctx.body = "Hello there"
+  ctx.status = 204;
+});
+
 router.get("/healthz", ctx => (ctx.status = 204));
+
 router.post("/leads", ctx => {
   logger.info({ lead: ctx.request.body });
   console.log({ lead: ctx.request.body });
